0de7820d986984872a5e9038692d656a4a23cd78
salvadormaurilio <salvadoredgar99@gmail.com>
2025-05-10 19:55:36 -0600

    Add new main activity DogsActivity with specify theme

---
+++ app/src/main/java/com/example/dogschallenge/ui/DogsActivity.kt
@@ -0,0 +1,19 @@
+package com.example.dogschallenge.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.dogschallenge.ui.theme.DogsChallengeTheme
+
+class DogsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DogsChallengeTheme {
+            }
+        }
+    }
+}
+
--- app/src/main/java/com/example/dogschallenge/ui/theme/Color.kt
+++ app/src/main/java/com/example/dogschallenge/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.example.dogschallenge.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val BluePrimary = Color(0xFF42A5F5)
+val BlueSecondary = Color(0xFF42A5F5)
+val LightBackground = Color(0xFFF5F5F5)
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val LightSurface = Color(0xFFFFFFFF)
+val DarkOnSurface = Color(0xCCFFFFFF)
+val LightOnSurface = Color(0xCC1E1E1E)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightPrimaryText = Color(0xFF333333)
+val DarkPrimaryText = Color(0xFFFFFFFF)
+val LightSecondaryText = Color(0xFF666666)
+val DarkSecondaryText = Color(0xFFFFFFFF)
+val LightTertiaryText = Color(0xFFF8F8F8)
+val DarkTertiaryText = Color(0xFFFFFFFF)
