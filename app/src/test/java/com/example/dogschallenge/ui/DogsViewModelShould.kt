9fbe380d99cc50ac3dd80d57b07bb2673186c288
salvadormaurilio <salvadoredgar99@gmail.com>
2025-05-10 19:09:30 -0600

    Add tests to DogsViewModel

---
+++ app/src/test/java/com/example/dogschallenge/ui/DogsViewModelShould.kt
@@ -0,0 +1,91 @@
+package com.example.dogschallenge.ui
+
+import com.example.dogschallenge.core.TestDispatcherRule
+import com.example.dogschallenge.core.assertThatEquals
+import com.example.dogschallenge.core.assertThatIsInstanceOf
+import com.example.dogschallenge.data.datasource.exception.DataException
+import com.example.dogschallenge.domain.GetDogsUseCase
+import com.example.dogschallenge.fakedata.givenDogsFakeData
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.kotlin.whenever
+
+class DogsViewModelShould {
+
+    @get:Rule
+    var testDispatcherRule = TestDispatcherRule()
+
+    private val getDogsUseCase = mock<GetDogsUseCase>()
+    private lateinit var dogsViewModel: DogsViewModel
+
+    @Before
+    fun setup() {
+        dogsViewModel = DogsViewModel(getDogsUseCase, testDispatcherRule.coroutinesDispatchers)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `Get Dogs data when initGetDogs is called and getDogs is success`() = runTest {
+        val dogs = givenDogsFakeData()
+        whenever(getDogsUseCase()).thenReturn(flowOf(Result.success(dogs)))
+
+        dogsViewModel.initGetDogs()
+        advanceUntilIdle()
+
+        val result = dogsViewModel.dogsUiState.firstOrNull()
+
+        verify(getDogsUseCase)()
+        assertThatEquals(result?.dogs, dogs)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `Get DogsException data when initGetDogs is called and getDogs is failure`() = runTest {
+        whenever(getDogsUseCase()).thenReturn(flowOf(Result.failure(DataException.DogsException())))
+
+        dogsViewModel.initGetDogs()
+        advanceUntilIdle()
+
+        val result = dogsViewModel.dogsUiState.firstOrNull()
+
+        verify(getDogsUseCase)()
+        assertThatIsInstanceOf<DataException.DogsException>(result?.error)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `Get Dogs data when retryGetDogs is called and getDogs is success`() = runTest {
+        val dogs = givenDogsFakeData()
+        whenever(getDogsUseCase()).thenReturn(flowOf(Result.success(dogs)))
+
+        dogsViewModel.retryGetDogs()
+        advanceUntilIdle()
+
+        val result = dogsViewModel.dogsUiState.firstOrNull()
+
+        verify(getDogsUseCase)()
+        assertThatEquals(result?.dogs, dogs)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `Get DogsException data when retryGetDogs is called and getDogs is failure`() = runTest {
+        whenever(getDogsUseCase()).thenReturn(flowOf(Result.failure(DataException.DogsException())))
+
+        dogsViewModel.retryGetDogs()
+        advanceUntilIdle()
+
+        val result = dogsViewModel.dogsUiState.firstOrNull()
+
+        verify(getDogsUseCase)()
+        assertThatIsInstanceOf<DataException.DogsException>(result?.error)
+    }
+}
